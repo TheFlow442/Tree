fa63014ae4977bad9cd0744353660e3c92c8edc9
James <shopifyproservice7@gmail.com>
2025-10-24 06:25:43 +0000

    https://console.firebase.google.com/u/3/project/smart-solar-agent/databa

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  "projectId": "studio-1685378276-702ff",
+  "projectId": "smart-solar-agent",
   "appId": "1:288585224465:web:75d024c6d910b8086a3cb0",
   "apiKey": "AIzaSyBUsB3M6UuHkAiv9FQO9z3uB-JQmfKOPlg",
-  "authDomain": "studio-1685378276-702ff.firebaseapp.com",
-  "databaseURL": "https://studio-1685378276-702ff-default-rtdb.firebaseio.com",
+  "authDomain": "smart-solar-agent.firebaseapp.com",
+  "databaseURL": "https://smart-solar-agent-default-rtdb.firebaseio.com",
   "measurementId": "",
   "messagingSenderId": "288585224465"
 };
