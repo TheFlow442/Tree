e7ef352bb5235d79c1d1080bb07dd15b4cd8cb60
Abubakar Saddiq <Abubakarsaddiq5001@gmail.com>
2025-10-25 14:22:54 +0000

    // app/layout.tsx export default function RootLayout({   children, }: {

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,5 @@
-
 import type { Metadata } from 'next';
 import './globals.css';
-import { cn } from '@/lib/utils';
-import { Toaster } from '@/components/ui/toaster';
-import { ThemeProvider } from '@/components/theme-provider';
-import { FirebaseClientProvider } from '@/firebase/client-provider';
 
 export const metadata: Metadata = {
   title: 'Solaris智控',
@@ -13,39 +8,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Space+Grotesk:wght@300..700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body
-        className={cn('font-body antialiased min-h-screen bg-background')}
+      <body 
+        className="font-body antialiased min-h-screen bg-background"
         suppressHydrationWarning={true}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <FirebaseClientProvider>
-            {children}
-          </FirebaseClientProvider>
-          <Toaster />
-        </ThemeProvider>
+        {children}
       </body>
     </html>
-  );
+  )
 }
