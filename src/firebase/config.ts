509682b65de9a307ab5d6abca3a1ddcb85f71e16
James <shopifyproservice7@gmail.com>
2025-10-29 07:59:32 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,12 @@
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  "projectId": "smart-solar-agent",
-  "appId": "1:288585224465:web:75d024c6d910b8086a3cb0",
-  "apiKey": "AIzaSyBUsB3M6UuHkAiv9FQO9z3uB-JQmfKOPlg",
-  "authDomain": "smart-solar-agent.firebaseapp.com",
-  "databaseURL": "https://smart-solar-agent-default-rtdb.firebaseio.com",
-  "measurementId": "",
-  "messagingSenderId": "288585224465"
+  apiKey: "AIzaSyBus4glxkSIDXh94Bp5W4SysUvWCpBMVYo",
+  authDomain: "smart-solar-agent.firebaseapp.com",
+  databaseURL: "https://smart-solar-agent-default-rtdb.firebaseio.com",
+  projectId: "smart-solar-agent",
+  storageBucket: "smart-solar-agent.firebasestorage.app",
+  messagingSenderId: "355748360131",
+  appId: "1:355748360131:web:73a07913df7c1362bf9776",
+  measurementId: "G-JEB94Z7W6K"
 };
